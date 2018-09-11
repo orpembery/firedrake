02066617754c21069802a631ca52213510e6f3c0
Lawrence Mitchell <lawrence@wence.uk>
2018-09-11 16:54:34 +0100

    Refactor preconditioner/matrix-free code layout
    Move preconditioner implementations into a separate submodule.

---
+++ firedrake/preconditioners/__init__.py
@@ -0,0 +1,5 @@
+from firedrake.preconditioners.base import *
+from firedrake.preconditioners.assembled import *
+from firedrake.preconditioners.massinv import *
+from firedrake.preconditioners.pcd import *
+from firedrake.preconditioners.patch import *
